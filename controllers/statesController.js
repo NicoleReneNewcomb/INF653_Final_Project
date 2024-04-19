51458c1dd8886376c684f17ae08c3d5f90c3c9d8
Nicole-Rene Newcomb <NicoleReneNewcomb@gmail.com>
2024-04-18 18:51:03 -0700

    Add Function to Serve JSON States Data
    Added function to serve the statesData.json file at the /states endpoint.

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -1,10 +1,17 @@
+const fs = require('fs').promises;
+const path = require('path');
 const States = require('../models/States');
 
 // get all state entries
 const getAllStates = async (req, res) => {
-    const states = await States.find();
-    if (!states) return res.status(204).json({ 'message': 'No states Found.' });
-    res.status(201).json(states);
+    try {
+        const data = await fs.readFile(path.join(__dirname, 'statesData.json'), 'utf8');
+        const states = JSON.parse(data);
+        res.json(states);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
 }
 
 module.exports = {
