197bdaff5cbff7726fed80e9b3fbb450790e52fc
Nicole-Rene Newcomb <NicoleReneNewcomb@gmail.com>
2024-04-18 17:50:00 -0700

    Added Routes for Non-Existing Pages
    Added routes for non-existing pages to show 404 page.

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -6,4 +6,16 @@ router.get('^/$|/css/style(.css)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'css', 'style.css'));
 });
 
+app.all('*', (req, res) => {
+    // serve custom 404 page when pages not found
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: "404 Not Found"});
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
+});
+
 module.exports = router;
