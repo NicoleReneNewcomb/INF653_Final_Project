0270f4723204bb383388b6fdefa73d2de5742c53
Nicole-Rene Newcomb <NicoleReneNewcomb@gmail.com>
2024-04-18 22:00:33 -0700

    Implemented Contiguous State Option
    Implemented contiguous state option.

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -5,15 +5,77 @@ const States = require('../models/States');
 // get all state entries
 const getAllStates = async (req, res) => {
     try {
+        // load state data from JSON file
         const data = await fs.readFile(path.join(__dirname, '..', 'statesData.json'), 'utf8');
         const states = JSON.parse(data);
-        res.json(states);
+
+        // check for contiq url query parameter
+        if (req.query.contig !== undefined) {
+            // array of non-contiguous state codes
+            const nonContiguousStates = ['AK', 'HI'];
+
+            const contig = req.query.contig.toLowerCase() === 'true';
+            const filteredStates = states.filter(state => {
+                const isContiguous = !nonContiguousStates.includes(state.code);
+                return isContiguous === contig;
+            });
+            return res.json(filteredStates);
+        }
+        else {
+            return res.json(states);
+        }
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Server error' });
     }
 }
 
+// get single state entry
+const getState = async (req, res) => {
+    try {
+        const data = await fs.readFile(path.join(__dirname, '..', 'statesData.json'), 'utf8');
+        const states = JSON.parse(data);
+        const state = states.find(state => state.code === req.params.stateCode.toUpperCase());
+    if (!state) {
+        return res.status(400).json({ "message": "Invalid state abbreviation parameter" });
+    }
+    res.json(state);
+    }
+    catch(err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+// create new fun fact
+const createNewFunFact = async (req, res) => {
+    const { funfacts } = req.body.funfacts;
+    const state = req.params.stateCode.toUpperCase();
+
+    if (!funfacts || !funfacts.length === 0) {
+        return res.status(400).json({ 'message': 'State fun facts value required' });
+    }
+
+    try {
+        const updatedState = await State.findOneAndUpdate(
+            { stateCode }, 
+            { $push: { funfacts: { $each: funfacts } } },
+            { new: true, upsert: true }
+        );
+
+        if (!updatedState) {
+            return res.status(404).json({ 'message': 'State not found' });
+        }
+
+        res.status(200).json(updatedState);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
 module.exports = {
-    getAllStates
+    getAllStates,
+    getState,
+    createNewFunFact
 };
--- routes/statesRoutes.js
+++ routes/statesRoutes.js
@@ -2,23 +2,18 @@ const express = require('express');
 const router = express.Router();
 const statesController = require('../controllers/statesController');
 
+// return all state data at /states/
 router.get('/', statesController.getAllStates);
-// router.get('*', (req, res) => {
-//     console.log("In StatesRoutes File");
-// });
 
-// // catch all for any non-existing directories/files
-// router.all('*', (req, res) => {
-//     // serve custom 404 page when pages not found
-//     console.log("ERROR HERE");
-//     res.status(404)
-//     if (req.accepts('html')) {
-//         res.sendFile(path.join(__dirname, '..', 'views', '404.html'));
-//     } else if (req.accepts('json')) {
-//         res.json({ error: "404 Not Found"});
-//     } else {
-//         res.type('txt').send('404 Not Found');
-//     }
-// });
+// pulling parameters from url
+router.route('/:stateCode')
+    .get(statesController.getState);
+
+// // .route() allows for chaining of different HTTP method types
+// router.route('/')
+// .get(employeesController.getAllEmployees)
+// .post(employeesController.createNewEmployee)
+// .put(employeesController.updateEmployee)
+// .delete(employeesController.deleteEmployee);
 
 module.exports = router;
